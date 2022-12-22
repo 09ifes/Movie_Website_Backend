be937719ab9f941249a1e5f060a7e41da89b3f00
Samuel Ife <samuelife@hotmail.co.uk>
2022-12-22 12:54:05 +0000

    sonarcloud setup

--- src/test/java/ActorTests.java
+++ src/test/java/ActorTests.java
@@ -1,2 +1,39 @@
+import UIElements.demo.Actor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+
 public class ActorTests {
+
+    @Test
+    public void getSetActorID(){
+        Random rand_x = new Random();
+        int x = rand_x.nextInt(1000);
+        Actor actor = new Actor();
+        actor.setActorID(x);
+        Assertions.assertEquals(x, actor.getActorID(), "Actor ID should be: " + x);
+    }
+
+    @Test
+    public void getSetFirstName(){
+        String firstName = "firstName";
+        Actor actor = new Actor();
+        actor.setFirstName(firstName);
+        Assertions.assertEquals(firstName, actor.getFirstName(), "Actor first name should be: " + firstName);
+    }
+
+    @Test
+    public void getSetLastName(){
+        String lastName = "lastName";
+        Actor actor = new Actor();
+        actor.setFirstName(lastName);
+        Assertions.assertEquals(lastName, actor.getFirstName(), "Actor last name should be: " + lastName);
+    }
+
+
+
+
+
 }
--- src/test/java/FilmTests.java
+++ src/test/java/FilmTests.java
@@ -7,13 +7,30 @@ import java.util.Random;
 public class FilmTests {
 
     @Test
-    public void testGETSetFilmID(){
+    public void testGetSetFilmID(){
         Random rand_x = new Random();
         int x = rand_x.nextInt(1000);
         Film film = new Film();
         film.setFilmID(x);
         Assertions.assertEquals(x, film.getFilmID(), "Film ID should be: " + x);
     }
+
+    @Test
+    public void testGetSetTitle(){
+        String title = "title";
+        Film film = new Film();
+        film.setTitle(title);
+        Assertions.assertEquals(title, film.getTitle(), "Title should be: " + title);
+    }
+
+    @Test
+    public void testGetSetDescription(){
+        String description = "description";
+        Film film = new Film();
+        film.setDescription(description);
+        Assertions.assertEquals(description, film.getDescription(), "Description should be: " + description);
+    }
+
 
 
 
